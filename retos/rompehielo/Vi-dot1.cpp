09619c91e49c6d073613a9d272ebf6d89bbfdceb
Vi-dot1 <andrenieves45@gmail.com>
2026-07-17 01:44:41 -0400

    solucion rompehielo, pequeñas correcciones al mensaje

--- retos/rompehielo/Vi-dot1.cpp
+++ retos/rompehielo/Vi-dot1.cpp
@@ -3,12 +3,12 @@
 // para compilar y ejecutar en linux: g++ Vi-dot1.cpp -o out; ./out
 // para Windows: siendo honesto olvide hace tiempo, si saben como actualizen esta linea!
 int main() {
-    std::cout << "Hellou, Soy Vi-dot1" << std::endl;
+    std::cout << "Hellou, soy Vi-dot1" << std::endl;
     std::cout << "llevo tiempo programando por hobby y aprendiendo por mi cuenta, ahorita estoy haciendo carrera como tal" << std::endl;
     std::cout << "--------------------------------------------------------------------------------------------------------------" << std::endl;
     std::cout << "Nunca me sentí la suficientmente comodo para hacer prs a proyectos y pienso tomar pasos a remediar eso" << std::endl;
     std::cout << "escribo esto en C++ porque decia cualquier lenguaje, pero si entiendo bien Python" << std::endl;
     
     // Para que de chance de leer
-    std::cin.getchar('');
+    std::getchar();
 }
