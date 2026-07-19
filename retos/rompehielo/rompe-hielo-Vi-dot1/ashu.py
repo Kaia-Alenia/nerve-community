cef7c57168ffe4589eee2e9e739d9f1fbbcb9ada
downstairs777 <minighost.pancakes@gmail.com>
2026-07-19 17:38:15 -0400

    feat: rompiendo el hielo

---
+++ retos/rompehielo/rompe-hielo-Vi-dot1/ashu.py
@@ -0,0 +1,11 @@
+def hello() -> None:
+    print("Hello, World! \n")
+    print("Soy Ashu (salud).")
+    print(
+        "Me gusta la informática y programar. Quiero conocer programadores con los que aprender y crear nuevos proyectos open source."
+    )
+    print("Me gustan los tiburones y el rock.")
+
+
+if __name__ == "__main__":
+    hello()
