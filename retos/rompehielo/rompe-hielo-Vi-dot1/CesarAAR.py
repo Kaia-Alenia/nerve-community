472ffd0ac4673e149fff40a0894382406bcd83c9
Kaia-Alenia <alejandrotrrsa9@gmail.com>
2026-07-19 13:06:50 -0600

    style: format code with black

--- retos/rompehielo/rompe-hielo-Vi-dot1/CesarAAR.py
+++ retos/rompehielo/rompe-hielo-Vi-dot1/CesarAAR.py
@@ -1,6 +1,9 @@
-
 def saludo():
     print("Hola. Soy CesarAAR.")
     print("Soy un apasionado de la IA (más que nada ML & DL) y el analisis de datos.")
-    print("Actualmente estoy aprendiendo a jugar ajedrez (de forma profesional) y trato de vincularlo con la IA y el analisis de datos.")
+    print(
+        "Actualmente estoy aprendiendo a jugar ajedrez (de forma profesional) y trato de vincularlo con la IA y el analisis de datos."
+    )
+
+
 saludo()
