1ab35e537d0ab6905322cc6ac950a7865ea7fb85
Tacos-1212 <sofia030jo@gmail.com>
2026-07-19 17:03:25 -0600

    style: format code whit black

--- retos/rompehielo/tacos.py
+++ retos/rompehielo/tacos.py
@@ -1,3 +1,5 @@
 print("Saludosssss desde termux")
 
-print("Soy tacos jajaj, estoy aqui para aprender a porgrmar y no ser un esclavo mas de la IA")
+print(
+    "Soy tacos jajaj, estoy aqui para aprender a porgrmar y no ser un esclavo mas de la IA"
+)
