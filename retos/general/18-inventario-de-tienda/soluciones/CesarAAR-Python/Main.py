b24b1f5b39025fcd8a0ba4d9cc2f759add5deddd
CesarAAR <cealalvarezr@gmail.com>
2026-07-23 15:47:45 -0700

    style: format code with black

--- retos/general/18-inventario-de-tienda/soluciones/CesarAAR-Python/Main.py
+++ retos/general/18-inventario-de-tienda/soluciones/CesarAAR-Python/Main.py
@@ -3,7 +3,7 @@ from Productos import Producto
 
 mi_inventario = Invetario()
 
-p1 = Producto("Test",10,5)
+p1 = Producto("Test", 10, 5)
 p2 = Producto("Mouse", 25.0, 10)
 
 mi_inventario.agregar_producto(p1)
@@ -11,7 +11,7 @@ mi_inventario.agregar_producto(p2)
 
 mi_inventario.mostrar_inventario()
 
-mi_inventario.actualizar_producto("Test",10,4)
+mi_inventario.actualizar_producto("Test", 10, 4)
 
 mi_inventario.mostrar_inventario()
 
--- retos/general/18-inventario-de-tienda/soluciones/CesarAAR-Python/Productos.py
+++ retos/general/18-inventario-de-tienda/soluciones/CesarAAR-Python/Productos.py
@@ -1,5 +1,5 @@
-#Clase Producto
-#Validaciones:
+# Clase Producto
+# Validaciones:
 """
 Nombre:
 - El campo no debe estar vacio.
@@ -14,14 +14,16 @@ Cantidad:
 - La cantidad debe de ser >= 0.
 """
 
-#NOTA:
+# NOTA:
 """
 No se agregó el deleter porque no le vi la logica de agregarlo.
 Bajo la logica de un negocio, el dueño no elimina el nombre|precio|cantidad, si va a eliminar algo es el producto completo,
 o si es el caso, solo actualizaría.
 """
+
+
 class Producto:
-    def __init__(self, nombre:str, precio:float, cantidad:int):
+    def __init__(self, nombre: str, precio: float, cantidad: int):
         self.nombre = nombre
         self.precio = precio
         self.cantidad = cantidad
@@ -29,7 +31,7 @@ class Producto:
     @property
     def nombre(self):
         return self.__nombre
-    
+
     @nombre.setter
     def nombre(self, new_nombre):
         if new_nombre.strip():
@@ -43,30 +45,32 @@ class Producto:
     @property
     def precio(self):
         return self.__precio
-    
+
     @precio.setter
     def precio(self, new_precio):
         if new_precio is None:
 
             raise ValueError("Campo Precio vacío")
         if new_precio < 0:
-                raise ValueError("Precio debe de ser mayor o igual a 0")
-        
+            raise ValueError("Precio debe de ser mayor o igual a 0")
+
         self.__precio = new_precio
 
     @property
     def cantidad(self):
         return self.__cantidad
-    
+
     @cantidad.setter
     def cantidad(self, new_cantidad):
         if new_cantidad is None:
             raise ValueError("Campo Cantidad vacío")
-        
+
         if new_cantidad < 0:
             raise ValueError("Cantidad debe de ser mayor o igual a 0.")
-        
+
         self.__cantidad = new_cantidad
 
     def mostrar_producto(self):
-        print(f"Nombre: {self.nombre}\nPrecio: {self.precio}\nCantidad Actual: {self.cantidad}")
+        print(
+            f"Nombre: {self.nombre}\nPrecio: {self.precio}\nCantidad Actual: {self.cantidad}"
+        )
